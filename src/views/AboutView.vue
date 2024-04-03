017eef7c363285898535813b90d83730cee655f0
aemanzano <manzano.amiel@nexseed.net>
2024-04-03 23:43:28 +0800

    added education pc and sp

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -23,6 +23,54 @@
         </div>
       </div>
     </div>
+    <div class="education-lg">
+      <h2 class="title">Education</h2>
+      <div class="box" v-for="educ in education" :key="educ.school">
+        <div class="left">
+          <div class="left-top">
+            <p class="year-title">Year</p>
+            <p class="year">{{ educ.year }}</p>
+            <div class="circle" />
+          </div>
+          <div class="left-bottom">
+            <img :src="educ.logo" alt="" class="logo" />
+            <div class="school">
+              <div class="name">{{ educ.school }}</div>
+              <div class="city">{{ educ.city }}</div>
+            </div>
+          </div>
+        </div>
+        <div class="right">
+          <p class="degree">{{ educ.degree }}</p>
+          <p class="activities">
+            {{ educ.activities }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="education-sm">
+      <h2 class="title">Education</h2>
+      <div class="box" v-for="educ in education" :key="educ.school">
+        <div class="year-section">
+          <p class="year-title">Year</p>
+          <p class="year">{{ educ.year }}</p>
+          <div class="circle" />
+        </div>
+        <div class="school-section">
+          <img :src="educ.logo" alt="" class="logo" />
+          <div class="school">
+            <div class="name">{{ educ.school }}</div>
+            <div class="city">{{ educ.city }}</div>
+          </div>
+        </div>
+        <div class="degree-section">
+          <p class="degree">{{ educ.degree }}</p>
+          <p class="activities">
+            {{ educ.activities }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,6 +103,26 @@ export default defineComponent({
           subTitle:
             "These accolades are the result of my diligence and enthusiasm towards my work.",
           image: require("@/assets/aboutme/card3.svg"),
+        },
+      ],
+      education: [
+        {
+          year: "2015 - 2020",
+          logo: require("@/assets/aboutme/usls.svg"),
+          school: "University of St. La Salle",
+          city: "Bacolod City",
+          degree: "Bachelor of Science Major in Computer Engineering",
+          activities:
+            "Activities and societies: Institute of Computer Engineers of the Philippines - USLS Secretary, Computer Hardware Interfacing and Programming Society Member, Engineering Society Academics Committee Member, Research on Computer Troubleshooting, Computer Engineering Department Best Thesis - The Development of Crop Management and Inventory System for Mushroom Farmers Institute of Computer Engineers of the Philippines - Region 6 1st Runner Up Quiz Bowl",
+        },
+        {
+          year: "2021",
+          logo: require("@/assets/aboutme/zuitt.svg"),
+          school: "Zuitt Coding Bootcamp",
+          city: "Quezon City",
+          degree: "Bootcamp Graduate",
+          activities:
+            "During my stay at Zuitt Bootcamp, we were taught and equipped with these different web development technologies: HTML, CSS, Javascript, Gitlab, Heroku, Vercel, MongoDB, NodeJS, ExpressJS, ReactJS, and NextJS. I graduated as top 1 in our batch.",
         },
       ],
     };
@@ -144,6 +212,120 @@ export default defineComponent({
       }
     }
   }
+
+  .education-lg {
+    padding: 118px 150px 105px;
+
+    .title {
+      font-size: 110px;
+      font-weight: 300;
+      line-height: 149.82px;
+      text-align: left;
+      color: var(--light-blue);
+      margin-bottom: 37px;
+    }
+
+    .box {
+      display: flex;
+
+      .left {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+
+        .left-top {
+          border-bottom: 3px solid #8f8f8f;
+          padding-bottom: 24px;
+          position: relative;
+
+          p {
+            text-align: left;
+            margin-left: 25px;
+            color: white;
+          }
+
+          .year-title {
+            font-size: 20px;
+            font-weight: 300;
+            line-height: 27.24px;
+            margin-bottom: 6px;
+          }
+
+          .year {
+            font-size: 35px;
+            font-weight: 300;
+            line-height: 47.67px;
+          }
+
+          .circle {
+            position: absolute;
+            bottom: -10px;
+            right: -10px;
+            background: #d9d9d9;
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+          }
+        }
+
+        .left-bottom {
+          display: flex;
+          align-items: center;
+
+          .logo {
+            margin: 20px 10px;
+          }
+
+          .school {
+            .name {
+              color: white;
+              font-size: 35px;
+              font-weight: 300;
+              line-height: 47.67px;
+              text-align: left;
+            }
+
+            .city {
+              font-size: 20px;
+              font-weight: 300;
+              line-height: 27.24px;
+              text-align: left;
+              color: var(--semi-dark-green);
+            }
+          }
+        }
+      }
+
+      .right {
+        flex: 1;
+        border-left: 3px solid #8f8f8f;
+
+        .degree {
+          font-size: 30px;
+          font-weight: 300;
+          line-height: 40.86px;
+          text-align: center;
+          color: var(--light-blue);
+          padding: 10px 48px 0;
+          width: 100%;
+        }
+
+        .activities {
+          padding: 20px 48px 150px;
+          width: 100%;
+          font-size: 15px;
+          font-weight: 300;
+          line-height: 22.5px;
+          text-align: justify;
+          color: white;
+        }
+      }
+    }
+  }
+
+  .education-sm {
+    display: none;
+  }
 }
 
 @media (max-width: 768px) {
@@ -218,6 +400,117 @@ export default defineComponent({
                 text-align: left;
               }
             }
+          }
+        }
+      }
+    }
+
+    .education-lg {
+      display: none;
+    }
+
+    .education-sm {
+      display: flex;
+      flex-direction: column;
+      padding: 40px 16px;
+
+      .title {
+        font-size: 60px;
+        font-weight: 300;
+        line-height: 81.72px;
+        text-align: left;
+        color: var(--light-blue);
+        margin-bottom: 22px;
+      }
+
+      .box {
+        .year-section {
+          padding-top: 54px;
+          text-align: left;
+          color: white;
+          padding-left: 22px;
+          padding-bottom: 20px;
+          border-bottom: 3px solid #8f8f8f;
+          border-left: 3px solid #8f8f8f;
+          position: relative;
+
+          .year-title {
+            font-size: 15px;
+            font-weight: 300;
+            line-height: 20.43px;
+          }
+
+          .year {
+            font-size: 25px;
+            font-weight: 300;
+            line-height: 34.05px;
+            text-align: left;
+          }
+
+          .circle {
+            position: absolute;
+            bottom: -10px;
+            left: -10px;
+            background: #d9d9d9;
+            width: 20px;
+            height: 20px;
+            border-radius: 50%;
+          }
+        }
+
+        .school-section {
+          display: flex;
+          padding: 20px;
+          border-left: 3px solid #8f8f8f;
+
+          .logo {
+            width: 60px;
+            margin-right: 10px;
+          }
+
+          .school {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            .name {
+              font-size: 25px;
+              font-weight: 300;
+              line-height: 34.05px;
+              text-align: left;
+              color: white;
+            }
+
+            .city {
+              font-size: 15px;
+              font-weight: 300;
+              line-height: 20.43px;
+              text-align: left;
+              color: var(--semi-dark-green);
+            }
+          }
+        }
+
+        .degree-section {
+          padding: 31px 20px 50px;
+          border-left: 3px solid #8f8f8f;
+
+          .degree {
+            font-family: Noto Sans;
+            font-size: 25px;
+            font-weight: 300;
+            line-height: 34.05px;
+            text-align: center;
+            color: var(--light-blue);
+          }
+
+          .activities {
+            margin-top: 30px;
+            font-size: 12px;
+            font-weight: 300;
+            line-height: 27.24px;
+            text-align: justify;
+            color: white;
           }
         }
       }
